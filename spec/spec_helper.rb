3beedcc6a5ca8773ad3dcfa5deba2b216e89bfdc
ikepon <tatsuyanoheya@gmail.com>
2017-11-25 22:26:00 +0900

    Add GaReportingV4 spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,5 +4,7 @@ require 'rails/all'
 require 'rspec/rails'
 require 'ga_reporting_v4'
 
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: ':memory:')
+
 RSpec.configure do |config|
 end
